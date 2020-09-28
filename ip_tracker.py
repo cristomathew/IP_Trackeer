0b1b82deee9749b74437d8bfe45048d63fae252b
overlord839 <cristomathew7@gmail.com>
2020-09-29 02:17:19 +0530

    ip_tracker

--- ip_tracker.py
+++ ip_tracker.py
@@ -13,13 +13,13 @@ from tqdm.notebook import tqdm_notebook
 def is_valid_ipv4_address(address):
     try:
         socket.inet_pton(socket.AF_INET, address)
-    except AttributeError:  # no inet_pton here, sorry
+    except AttributeError:  
         try:
             socket.inet_aton(address)
         except socket.error:
             return False
         return address.count('.') == 3
-    except socket.error:  # not a valid address
+    except socket.error:  
         return False
 
     return True
@@ -27,7 +27,7 @@ def is_valid_ipv4_address(address):
 def is_valid_ipv6_address(address):
     try:
         socket.inet_pton(socket.AF_INET6, address)
-    except socket.error:  # not a valid address
+    except socket.error:  
         return False
     return True
 
@@ -57,5 +57,6 @@ if check_ip(city):
     plot(response.location.latitude,response.location.longitude)
 else:
     print("wrong ip")
+
 print("\n***END***\n")
 reader.close()
